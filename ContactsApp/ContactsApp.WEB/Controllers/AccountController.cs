276d648446e730f0978a0136e0d17ec5ceda0a03
Danis Yakupov <waitdic161616@gmail.com>
2020-10-02 10:34:44 +0700

    Add Web project

---
+++ ContactsApp/ContactsApp.WEB/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsApp.WEB.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return null;
+        }
+    }
+}
