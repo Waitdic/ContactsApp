8762a93fc45b260e406a2486e4eb08a0a2c89a4b
Danis Yakupov <waitdic161616@gmail.com>
2020-10-30 09:25:52 +0700

    Test

---
+++ ContactsApp/UnitTests/AccountControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ContactsApp.BLL.Models;
+using Xunit;
+
+namespace UnitTests
+{
+    public class AccountControllerTests
+    {
+        [Fact]
+        public void AddNewContactTest()
+        {
+            // Arrange
+            var manager = new ContactManager();
+
+            // Act
+            bool check;
+            try
+            {
+                manager.AddContact(AddNewContact());
+                check = true;
+            }
+            catch (Exception)
+            {
+                check = false;
+            }
+
+            Assert.True(check);
+        }
+
+        private Contact AddNewContact()
+        {
+            return new Contact
+            {
+                Name = "Name" + Guid.NewGuid().ToString(),
+                Surname = "Surname" + Guid.NewGuid().ToString(),
+                Birthday = DateTime.Now.Date,
+                Phone = "8-111-111-11-11",
+                Vk = "daniska1616",
+                Email = "danis161616@yandex.ru",
+            };
+        }
+    }
+}
