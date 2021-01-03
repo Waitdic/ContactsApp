b44be9f037401120582f235ec1a33033261bd149
Danis Yakupov <waitdic161616@gmail.com>
2021-01-04 02:44:34 +0700

    Добавил отдельные модельки для модального окна

--- ContactsApp/ContactsApp.WEB/ClientApp/src/app/app.module.ts
+++ ContactsApp/ContactsApp.WEB/ClientApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, HttpClientModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    declarations: [AppComponent, ModalComponent],
+    bootstrap: [AppComponent],
+    entryComponents: [ModalComponent]
 })
 export class AppModule { }
